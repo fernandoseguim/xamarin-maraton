681cdb6128295dc5cd569e6c5fea9f1d31785e96
Fernando Seguim <fernando.seguim@gmail.com>
2017-03-07 22:04:06 -0300

    pcl vs sap

---
+++ Greetings/Greetings/GreetingsPage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Greetings
+{
+	public class GreetingsPage : ContentPage
+	{
+		public GreetingsPage()
+		{
+
+			var MyLabel = new Label();
+			MyLabel.Text = "Greetings, Xamarin.Forms!";
+			this.Content = MyLabel;
+
+			Padding = new Thickness(0, 20, 0, 0);
+
+		}
+	}
+}
+
---
+++ GreetingsSAP/GreetingsSAP/GreetingsSAPPage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace GreetingsSAP
+{
+	public class GreetingsSAPPage : ContentPage
+	{
+		public GreetingsSAPPage()
+		{
+			var MyLabel = new Label();
+			MyLabel.Text = "Greetings, Xamarin.Forms!";
+			this.Content = MyLabel;
+
+			//#if __IOS__
+			//			Padding = new Thickness(0, 20, 0, 0);
+			//#endif
+
+			//		Padding = new Thickness(0, Device.OnPlatform(20, 0, 0), 0, 0);
+
+
+			//Device.OnPlatform(iOS: () =>
+			//{
+
+			//	Padding = new Thickness(0, 20, 0, 0);
+
+			//});
+
+			//MyLabel.HorizontalOptions = LayoutOptions.Center;
+			//MyLabel.VerticalOptions = LayoutOptions.Center;
+
+			MyLabel.HorizontalTextAlignment = TextAlignment.Center;
+			MyLabel.VerticalTextAlignment = TextAlignment.Center;
+
+		}
+	}
+}
+
