8bfe64ed8342f167beb0ae246a93dc8e701578bc
Fernando Seguim <fernando.seguim@gmail.com>
2017-03-07 21:58:44 -0300

    first project Xamarin Android

---
+++ HelloXamarinAndroid/HelloXamarinAndroid/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace HelloXamarinAndroid
+{
+	[Activity(Label = "HelloXamarinAndroid", MainLauncher = true)]
+	public class MainActivity : Activity
+	{
+		Button btnHelloWorld;
+		TextView txtHelloWorld;
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			btnHelloWorld = FindViewById<Button>(Resource.Id.btn_helloworld);
+			txtHelloWorld = FindViewById<TextView>(Resource.Id.txt_helloworld);
+
+			btnHelloWorld.Click += delegate {
+
+				txtHelloWorld.Text = "Welcome to Xamarin World, this is your first Android App build with Xamarin Studio"; 
+
+			};
+
+		}
+
+		 
+	}
+}
+
