5a96e0314fb3a1f9c25ea52ddc34c6b37c50f587
Fernando Seguim <fernando.seguim@gmail.com>
2017-03-07 22:02:41 -0300

    lab03 - hands-on Portable Class Library

---
+++ HelloXamarinFormsPCL/HelloXamarinFormsPCL/GreetingPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace HelloXamarinFormsPCL
+{
+	public class GreetingPage : ContentPage
+	{
+		public GreetingPage()
+		{
+			Content = new StackLayout
+			{
+				Children = {
+					new Label { Text = "Hello ContentPage" }
+				}
+			};
+		}
+	}
+}
+
