a63b23ce781656cb4abbccb60415d21778ade196
Fernando Seguim <fernando.seguim@gmail.com>
2017-03-07 21:57:11 -0300

    Sample use Shared Project with Xamarin

---
+++ DemoSharedProject/DemoSharedProject.Shared/Printer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace DemoFileLinked.Core
+{
+	public static class Printer
+	{
+		public static void ShowMessage()
+		{
+			Console.WriteLine("Hello Xamarin, this is a sample of shared project");
+#if __ANDROID__
+			Console.WriteLine("This app is running with Android SDK version: " + Android.OS.Build.VERSION.Sdk);
+#elif __IOS__
+			Console.WriteLine("This app is running with iOS SDK version: " + UIKit.UIDevice.CurrentDevice.SystemVersion);
+#endif
+
+		}
+	}
+}
---
+++ DemoSharedProject/DemoSharedProject/MyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace DemoSharedProject
+{
+	public class MyClass
+	{
+		public MyClass()
+		{
+		}
+	}
+}
